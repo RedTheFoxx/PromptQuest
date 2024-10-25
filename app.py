9734c92be5a73a0813f60f99ba9f2961543ef88a
Red <morisseau.jeremy@gmail.com>
2024-10-25 18:09:29 +0200

    gradio import

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from modules.llm_router import LLMConnector
+import gradio as gr
 import os
 
-# llm_connector = LLMConnector(api_key=os.getenv("OPENROUTER_API_KEY"), site_url="", app_name="PromptQuest")
+# Get Claude
+llm_connector = LLMConnector(api_key=os.getenv("OPENROUTER_API_KEY"), site_url="", app_name="PromptQuest")
+
+# Build the interface
+pass
